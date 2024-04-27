16272d81e8cc9f264da81276e8fa2dd4a217e40f
diamondxorb <168246513+diamondxorb@users.noreply.github.com>
2024-04-26 23:15:44 -0300

    Create  2413 - Busca na Internet.c

---
+++ 2413 - Busca na Internet.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    int terceiro_link, primeiro_link;
+    1<=terceiro_link<=1000;
+    scanf("%d\n", &terceiro_link);
+    primeiro_link = terceiro_link*4;
+    printf("%d\n", primeiro_link);
+    return 0;
+}
