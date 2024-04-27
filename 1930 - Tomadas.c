7bdb675b180d56386130d33d447f53ccb72d06fb
diamondxorb <168246513+diamondxorb@users.noreply.github.com>
2024-04-26 23:15:12 -0300

    Create  1930 - Tomadas.c

---
+++ 1930 - Tomadas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    int t1, t2, t3, t4, aparelhos;
+    2<=t1, t2, t3, t4<=6;
+    scanf("%d %d %d %d\n", &t1, &t2, &t3, &t4);
+    aparelhos = (t1+t2+t3+t4) - 3;
+    printf("%d\n", aparelhos);
+    return 0;
+}
