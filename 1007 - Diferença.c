3806da101046503ee6c4f36f761cc810b207d196
diamondxorb <168246513+diamondxorb@users.noreply.github.com>
2024-04-26 23:00:22 -0300

    Create  1007 - Diferença.c

---
+++ 1007 - Diferença.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    int a, b, c, d, diferenca;
+    scanf("%d\n", &a);
+    scanf("%d\n", &b);
+    scanf("%d\n", &c);
+    scanf("%d\n", &d);
+    DIFERENCA = (a*b - c*d);
+    printf("DIFERENCA = %d\n", diferenca);
+    return 0;
+}
