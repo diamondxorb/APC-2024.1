35dab415aacc54e7b26394982c2a3790dfed1bea
Júlia <168246513+diamondxorb@users.noreply.github.com>
2024-05-03 16:40:26 -0300

    Update 1146 - Sequências Crescentes.c

--- 1146 - Sequências Crescentes.c
+++ 1146 - Sequências Crescentes.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 int main () {
-    int i, numero, x;
-    scanf("%d\n", &x);
-    numero = 1;
-    for(i=1; i<x; i++) {
-        printf("%d ", numero);
-        numero++;
-    }
-    printf("%d\n", x);
-    while(x!=0) {
-        scanf("%d\n", &x);
-        numero = 1;
-        for(i=1; i<x; i++) {
-            printf("%d ", numero);
-            numero++;
+    int i, numero, contagem;
+    while(1) {
+        scanf("%d", &numero);
+        if(numero==0) {
+            break;
         }
-        if(x!=0) {
-            printf("%d\n", x);
+        contagem = 1;
+        for(i=1; i<numero; i++) {
+            printf("%d ", contagem);
+            contagem++;
         }
+        printf("%d\n", numero);
     }
     return 0;
 }
