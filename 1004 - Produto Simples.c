67d96576bdc654f2206d43c681ee14c33478e161
Júlia <168246513+diamondxorb@users.noreply.github.com>
2024-05-03 15:47:37 -0300

    Create  1004 - Produto Simples.c

---
+++ 1004 - Produto Simples.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    int a, b, produto;
+    scanf("%d\n", &a);
+    scanf("%d\n", &b);
+    produto = (a*b);
+    printf("PROD = %d\n", produto);
+    return 0;
+}
