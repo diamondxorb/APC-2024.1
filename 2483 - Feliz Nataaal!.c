8f53a45bd1897e668d3989290a99254a320aacb7
Júlia <168246513+diamondxorb@users.noreply.github.com>
2024-05-03 15:36:28 -0300

    Create  2483 - Feliz Nataaal!.c

---
+++ 2483 - Feliz Nataaal!.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    int i, numero;
+    scanf("%d", &numero);
+    printf("Feliz nat");
+    for(i=1;i<=numero;i++) {
+        printf("a");
+    }
+    printf("l!\n");
+    return 0;
+}
