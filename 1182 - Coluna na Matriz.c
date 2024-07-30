76bb01ebab0ffcd0a595b76b6d13141e9cbe3e9f
Júlia <168246513+diamondxorb@users.noreply.github.com>
2024-07-30 17:10:13 -0300

    Update 1182 - Coluna na Matriz.c

--- 1182 - Coluna na Matriz.c
+++ 1182 - Coluna na Matriz.c
@@ -9,8 +9,7 @@ int main () {
   scanf("%d", &numero);
   scanf(" %c", &operacao);
   for(i=0; i<12; i++) {
-    for(j=0; j<12
-; j++) {
+    for(j=0; j<12; j++) {
       scanf("%lf", &matriz[i][j]);
     }
   }
