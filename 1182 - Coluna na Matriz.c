11e6dc4ca828193ac41760ead9557a1af12ece24
Júlia <168246513+diamondxorb@users.noreply.github.com>
2024-07-30 17:08:24 -0300

    Create 1182 - Coluna na Matriz.c

---
+++ 1182 - Coluna na Matriz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main () {
+  int i, j, numero;
+  double soma, media, matriz[12][12];
+  char operacao;
+  soma = 0;
+  media = 0;
+  scanf("%d", &numero);
+  scanf(" %c", &operacao);
+  for(i=0; i<12; i++) {
+    for(j=0; j<12
+; j++) {
+      scanf("%lf", &matriz[i][j]);
+    }
+  }
+  for(j=0; j<12; j++) {
+      soma+=matriz[numero][j];
+  }
+  media = soma/12.0;
+  if(operacao=='S') {
+    printf("%.1lf\n", soma);
+  }
+  else {
+    printf("%.1lf\n", media);
+  }
+  return 0;
+}
