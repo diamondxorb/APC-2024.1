60c1f88852e2dc1d79a2334fae171526707ed80f
Júlia <168246513+diamondxorb@users.noreply.github.com>
2024-08-14 08:50:47 -0300

    Create 1187 - Área Superior.c

---
+++ 1187 - Área Superior.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main () {
+  int i, j, contagem, variavel;
+  double soma, media, matriz[12][12];
+  char operacao;
+  soma = 0;
+  media = 0;
+  contagem = 0;
+  variavel = 1;
+  scanf(" %c", &operacao);
+  for(i=0; i<12; i++) {
+    for(j=0; j<12; j++) {
+      scanf("%lf", &matriz[i][j]);
+      if(i+j<11 && i+j>=variavel) {
+          soma+=matriz[i][j];
+          contagem++;
+      }
+    }
+    variavel+=2;
+    if(i==4) {
+      break;
+    }
+  }
+  media = soma/contagem;
+  if(operacao=='S') {
+      printf("%.1lf\n", soma);
+  }
+  else {
+      printf("%.1lf\n", media);
+  }
+  return 0;
+}
